df316e8b8980284e3fe0f20a1fcd88dc06323ff5
Julien Levesy <jlevesy@gmail.com>
2022-08-02 23:05:56 +0200

    feat: implement create escalation admission webhook

---
+++ webhook/escalation/webhook.go
@@ -0,0 +1,261 @@
+package escalation
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+
+	admissionv1 "k8s.io/api/admission/v1"
+	authenticationv1 "k8s.io/api/authentication/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/klog/v2"
+
+	kudo "github.com/jlevesy/kudo/pkg/apis/k8s.kudo.dev"
+	"github.com/jlevesy/kudo/pkg/apis/k8s.kudo.dev/v1alpha1"
+	kudov1alpha1 "github.com/jlevesy/kudo/pkg/apis/k8s.kudo.dev/v1alpha1"
+	"github.com/jlevesy/kudo/pkg/generics"
+	"github.com/jlevesy/kudo/pkg/webhooksupport"
+)
+
+var (
+	ExpectedKind = metav1.GroupVersionKind{
+		Group:   kudo.GroupName,
+		Version: kudov1alpha1.Version,
+		Kind:    kudov1alpha1.KindEscalation,
+	}
+
+	UnexpectedErrorStatus = metav1.Status{
+		Status:  metav1.StatusFailure,
+		Message: "Unexpected error, see controller logs for details",
+	}
+)
+
+type EscalationPoliciesGetter interface {
+	Get(ctx context.Context, name string, opts metav1.GetOptions) (*v1alpha1.EscalationPolicy, error)
+}
+
+type WebhookHandler struct {
+	policiesGetter EscalationPoliciesGetter
+}
+
+func NewWebhookHandler(g EscalationPoliciesGetter) *WebhookHandler {
+	return &WebhookHandler{policiesGetter: g}
+}
+
+func (h *WebhookHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	review := admissionv1.AdmissionReview{
+		Response: &admissionv1.AdmissionResponse{},
+	}
+
+	klog.Info("Handing a mutation request")
+
+	if err := json.NewDecoder(r.Body).Decode(&review); err != nil {
+		klog.ErrorS(err, "Unable to decode webhook request")
+		review.Response.Result = &UnexpectedErrorStatus
+
+		// Can't really say more here, as I don't have the admission request.
+		webhooksupport.WriteJSON(rw, http.StatusBadRequest, &review)
+		return
+	}
+
+	if review.Request == nil {
+		klog.Error("Received malformed review payload without any request")
+		review.Response.Result = &UnexpectedErrorStatus
+
+		webhooksupport.WriteJSON(rw, http.StatusOK, &review)
+		return
+	}
+
+	review.Response.UID = review.Request.UID
+
+	if review.Request.Kind != ExpectedKind {
+		klog.Errorf(
+			"Received unexpected review kind %q for user %q",
+			review.Request.Kind,
+			review.Request.UserInfo.Username,
+		)
+
+		review.Response.Result = &UnexpectedErrorStatus
+
+		webhooksupport.WriteJSON(rw, http.StatusOK, &review)
+		return
+	}
+
+	if review.Request.Operation != admissionv1.Create {
+		klog.Errorf(
+			"Received unexpected operation %q for user %q",
+			review.Request.Operation,
+			review.Request.UserInfo.Username,
+		)
+
+		review.Response.Result = &UnexpectedErrorStatus
+
+		webhooksupport.WriteJSON(rw, http.StatusOK, &review)
+		return
+	}
+
+	var escalation kudov1alpha1.Escalation
+
+	if err := json.Unmarshal(review.Request.Object.Raw, &escalation); err != nil {
+		klog.ErrorS(err, "Can't unmarhal created object")
+
+		review.Response.Result = &UnexpectedErrorStatus
+
+		webhooksupport.WriteJSON(rw, http.StatusOK, &review)
+		return
+	}
+
+	if strings.TrimSpace(escalation.Spec.Reason) == "" {
+		klog.InfoS(
+			"User submitted an escalation request without any reason",
+			"username",
+			review.Request.UserInfo.Username,
+		)
+
+		review.Response.Result = &metav1.Status{
+			Status:  metav1.StatusFailure,
+			Message: "Please provide a reason for your escalation request",
+		}
+
+		webhooksupport.WriteJSON(rw, http.StatusOK, &review)
+		return
+
+	}
+
+	policy, err := h.policiesGetter.Get(
+		r.Context(),
+		escalation.Spec.PolicyName,
+		metav1.GetOptions{},
+	)
+
+	switch {
+	case errors.IsNotFound(err):
+		klog.InfoS(
+			"User submitted an escalation request refering to a policy that doesn't exist",
+			usernameAndPolicyTags(
+				review.Request.UserInfo.Username,
+				"unknown",
+			)...,
+		)
+
+		review.Response.Result = &metav1.Status{
+			Status:  metav1.StatusFailure,
+			Message: fmt.Sprintf("Unknown policy: %s", escalation.Spec.PolicyName),
+		}
+
+		webhooksupport.WriteJSON(rw, http.StatusOK, &review)
+		return
+	case err != nil:
+		klog.ErrorS(err, "Unable to retrieve escalation policy")
+		review.Response.Result = &UnexpectedErrorStatus
+
+		webhooksupport.WriteJSON(rw, http.StatusOK, &review)
+		return
+	default:
+		// We're good.
+	}
+
+	if !userAllowed(*policy, review.Request.UserInfo) {
+		klog.InfoS(
+			"User attempted to use an escalation policy, but is not part of the policy subjects",
+			usernameAndPolicyTags(
+				review.Request.UserInfo.Username,
+				policy.Name,
+			)...,
+		)
+
+		review.Response.Result = &metav1.Status{
+			Status: metav1.StatusFailure,
+			Message: fmt.Sprintf(
+				"User %q is not allowed to use the escalation policy %q",
+				review.Request.UserInfo.Username,
+				policy.Name,
+			),
+		}
+
+		webhooksupport.WriteJSON(rw, http.StatusOK, &review)
+		return
+	}
+
+	if review.Response.Patch, err = genObjectPatch(review.Request.UserInfo); err != nil {
+		klog.ErrorS(
+			err,
+			"Unable to generate object patch",
+			usernameAndPolicyTags(
+				review.Request.UserInfo.Username,
+				policy.Name,
+			)...,
+		)
+
+		review.Response.Result = &UnexpectedErrorStatus
+
+		webhooksupport.WriteJSON(rw, http.StatusOK, &review)
+		return
+
+	}
+
+	klog.InfoS(
+		"User submitted an escalation request",
+		"username",
+		review.Request.UserInfo.Username,
+		"policy",
+		escalation.Spec.PolicyName,
+	)
+
+	review.Response.Allowed = true
+	review.Response.Result = &metav1.Status{Status: metav1.StatusSuccess}
+	review.Response.PatchType = generics.Ptr(admissionv1.PatchTypeJSONPatch)
+
+	webhooksupport.WriteJSON(rw, http.StatusOK, &review)
+}
+
+// userAllowed returns true when an user is allowed to use an escalation policy based
+// on the policy subjects.
+// An user is allowed if and only if one of the policy subject:
+// - is of Kind user with the same username.
+// - is of Kind group and the user belongs to this group.
+func userAllowed(policy kudov1alpha1.EscalationPolicy, user authenticationv1.UserInfo) bool {
+	for _, subject := range policy.Spec.Subjects {
+		switch subject.Kind {
+		case rbacv1.GroupKind:
+			if generics.Contains(user.Groups, subject.Name) {
+				return true
+			}
+		case rbacv1.UserKind:
+			if subject.Name == user.Username {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func genObjectPatch(user authenticationv1.UserInfo) ([]byte, error) {
+	patch := []struct {
+		Op    string `json:"op"`
+		Path  string `json:"path"`
+		Value string `json:"value"`
+	}{
+		{
+			Op:    "replace",
+			Path:  "/spec/submittedBy",
+			Value: user.Username,
+		},
+	}
+
+	return json.Marshal(&patch)
+}
+
+func usernameAndPolicyTags(username, policy string) []any {
+	return []any{
+		"username",
+		username,
+		"policy",
+		policy,
+	}
+}
---
+++ webhook/escalation/webhook_test.go
@@ -0,0 +1,356 @@
+package escalation_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	admissionv1 "k8s.io/api/admission/v1"
+	authenticationv1 "k8s.io/api/authentication/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	kudov1alpha1 "github.com/jlevesy/kudo/pkg/apis/k8s.kudo.dev/v1alpha1"
+	"github.com/jlevesy/kudo/pkg/generated/clientset/versioned/fake"
+	"github.com/jlevesy/kudo/pkg/generics"
+	"github.com/jlevesy/kudo/webhook/escalation"
+)
+
+const requestUID = "request-uid"
+
+var (
+	k8sStateFixtures = []runtime.Object{
+		&kudov1alpha1.EscalationPolicy{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       kudov1alpha1.KindEscalationPolicy,
+				APIVersion: kudov1alpha1.SchemeGroupVersion.String(),
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "policy-1",
+			},
+			Spec: kudov1alpha1.EscalationPolicySpec{
+				Subjects: []rbacv1.Subject{
+					{
+						Kind: rbacv1.GroupKind,
+						Name: "group-b@org.com",
+					},
+					{
+						Kind: rbacv1.UserKind,
+						Name: "user-c",
+					},
+				},
+			},
+		},
+	}
+
+	unexpectedErrorReview = admissionv1.AdmissionReview{
+		Response: &admissionv1.AdmissionResponse{
+			Allowed: false,
+			Result:  &escalation.UnexpectedErrorStatus,
+		},
+	}
+
+	unexpectedErrorReviewWithUID = admissionv1.AdmissionReview{
+		Response: &admissionv1.AdmissionResponse{
+			UID:     requestUID,
+			Allowed: false,
+			Result:  &escalation.UnexpectedErrorStatus,
+		},
+	}
+)
+
+func TestEscalationWebhookHandler_ServeHTTP(t *testing.T) {
+	testCases := []struct {
+		desc        string
+		requestBody io.Reader
+
+		wantStatusCode int
+		wantReview     admissionv1.AdmissionReview
+	}{
+		{
+			desc:           "raises bad request when request body is not JSON",
+			requestBody:    bytes.NewBuffer([]byte("notjson")),
+			wantStatusCode: http.StatusBadRequest,
+			wantReview:     unexpectedErrorReview,
+		},
+		{
+			desc: "raises an error if request is missing",
+			requestBody: encodeObject(
+				t,
+				admissionv1.AdmissionReview{},
+			),
+			wantStatusCode: http.StatusOK,
+			wantReview:     unexpectedErrorReview,
+		},
+		{
+			desc: "raises an error if kind is unexpected",
+			requestBody: encodeObject(
+				t,
+				admissionv1.AdmissionReview{
+					Request: &admissionv1.AdmissionRequest{
+						UID: requestUID,
+						Kind: metav1.GroupVersionKind{
+							Group:   "something.bar",
+							Version: "v1",
+							Kind:    "Something",
+						},
+					},
+				},
+			),
+			wantStatusCode: http.StatusOK,
+			wantReview:     unexpectedErrorReviewWithUID,
+		},
+		{
+			desc: "raises an error if operation is not CREATE",
+			requestBody: encodeObject(
+				t,
+				admissionv1.AdmissionReview{
+					Request: &admissionv1.AdmissionRequest{
+						UID:       requestUID,
+						Kind:      escalation.ExpectedKind,
+						Operation: admissionv1.Delete,
+					},
+				},
+			),
+			wantStatusCode: http.StatusOK,
+			wantReview:     unexpectedErrorReviewWithUID,
+		},
+		{
+			desc: "raises an error if the policy is not known",
+			requestBody: encodeObject(
+				t,
+				admissionv1.AdmissionReview{
+					Request: &admissionv1.AdmissionRequest{
+						UID:       requestUID,
+						Kind:      escalation.ExpectedKind,
+						Operation: admissionv1.Create,
+						Object: runtime.RawExtension{
+							Raw: encodeObject(
+								t,
+								kudov1alpha1.Escalation{
+									Spec: kudov1alpha1.EscalationSpec{
+										PolicyName: "some-unknown-policy",
+										Reason:     "unlimited POWERRRRRRR!",
+									},
+								},
+							).Bytes(),
+						},
+					},
+				},
+			),
+			wantStatusCode: http.StatusOK,
+			wantReview: admissionv1.AdmissionReview{
+				Response: &admissionv1.AdmissionResponse{
+					UID:     requestUID,
+					Allowed: false,
+					Result: &metav1.Status{
+						Status:  metav1.StatusFailure,
+						Message: "Unknown policy: some-unknown-policy",
+					},
+				},
+			},
+		},
+		{
+			desc: "raises an error if the escalation has no reason",
+			requestBody: encodeObject(
+				t,
+				admissionv1.AdmissionReview{
+					Request: &admissionv1.AdmissionRequest{
+						UID:       requestUID,
+						Kind:      escalation.ExpectedKind,
+						Operation: admissionv1.Create,
+						Object: runtime.RawExtension{
+							Raw: encodeObject(
+								t,
+								kudov1alpha1.Escalation{
+									Spec: kudov1alpha1.EscalationSpec{
+										PolicyName: "policy-1",
+									},
+								},
+							).Bytes(),
+						},
+						UserInfo: authenticationv1.UserInfo{
+							Username: "user",
+						},
+					},
+				},
+			),
+			wantStatusCode: http.StatusOK,
+			wantReview: admissionv1.AdmissionReview{
+				Response: &admissionv1.AdmissionResponse{
+					UID:     requestUID,
+					Allowed: false,
+					Result: &metav1.Status{
+						Status:  metav1.StatusFailure,
+						Message: "Please provide a reason for your escalation request",
+					},
+				},
+			},
+		},
+		{
+			desc: "raises an error if the user is not allowed to use the policy",
+			requestBody: encodeObject(
+				t,
+				admissionv1.AdmissionReview{
+					Request: &admissionv1.AdmissionRequest{
+						UID:       requestUID,
+						Kind:      escalation.ExpectedKind,
+						Operation: admissionv1.Create,
+						Object: runtime.RawExtension{
+							Raw: encodeObject(
+								t,
+								kudov1alpha1.Escalation{
+									Spec: kudov1alpha1.EscalationSpec{
+										PolicyName: "policy-1",
+										Reason:     "I need moar power",
+									},
+								},
+							).Bytes(),
+						},
+						UserInfo: authenticationv1.UserInfo{
+							Username: "user",
+						},
+					},
+				},
+			),
+			wantStatusCode: http.StatusOK,
+			wantReview: admissionv1.AdmissionReview{
+				Response: &admissionv1.AdmissionResponse{
+					UID:     requestUID,
+					Allowed: false,
+					Result: &metav1.Status{
+						Status:  metav1.StatusFailure,
+						Message: "User \"user\" is not allowed to use the escalation policy \"policy-1\"",
+					},
+				},
+			},
+		},
+		{
+			desc: "allows users by username",
+			requestBody: encodeObject(
+				t,
+				admissionv1.AdmissionReview{
+					Request: &admissionv1.AdmissionRequest{
+						UID:       requestUID,
+						Kind:      escalation.ExpectedKind,
+						Operation: admissionv1.Create,
+						Object: runtime.RawExtension{
+							Raw: encodeObject(
+								t,
+								kudov1alpha1.Escalation{
+									Spec: kudov1alpha1.EscalationSpec{
+										PolicyName: "policy-1",
+										Reason:     "I need moar power",
+									},
+								},
+							).Bytes(),
+						},
+						UserInfo: authenticationv1.UserInfo{
+							Username: "user-c",
+						},
+					},
+				},
+			),
+			wantStatusCode: http.StatusOK,
+			wantReview: admissionv1.AdmissionReview{
+				Response: &admissionv1.AdmissionResponse{
+					UID:       requestUID,
+					Allowed:   true,
+					Result:    &metav1.Status{Status: metav1.StatusSuccess},
+					PatchType: generics.Ptr(admissionv1.PatchTypeJSONPatch),
+					Patch:     []byte(`[{"op":"replace","path":"/spec/submittedBy","value":"user-c"}]`),
+				},
+			},
+		},
+		{
+			desc: "allows users by group membership",
+			requestBody: encodeObject(
+				t,
+				admissionv1.AdmissionReview{
+					Request: &admissionv1.AdmissionRequest{
+						UID:       requestUID,
+						Kind:      escalation.ExpectedKind,
+						Operation: admissionv1.Create,
+						Object: runtime.RawExtension{
+							Raw: encodeObject(
+								t,
+								kudov1alpha1.Escalation{
+									Spec: kudov1alpha1.EscalationSpec{
+										PolicyName: "policy-1",
+										Reason:     "I need moar power",
+									},
+								},
+							).Bytes(),
+						},
+						UserInfo: authenticationv1.UserInfo{
+							Username: "user-b",
+							Groups:   []string{"group-b@org.com"},
+						},
+					},
+				},
+			),
+			wantStatusCode: http.StatusOK,
+			wantReview: admissionv1.AdmissionReview{
+				Response: &admissionv1.AdmissionResponse{
+					UID:       requestUID,
+					Allowed:   true,
+					Result:    &metav1.Status{Status: metav1.StatusSuccess},
+					PatchType: generics.Ptr(admissionv1.PatchTypeJSONPatch),
+					Patch:     []byte(`[{"op":"replace","path":"/spec/submittedBy","value":"user-b"}]`),
+				},
+			},
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			var (
+				responseWriter = httptest.NewRecorder()
+				request        = httptest.NewRequest(
+					http.MethodPost,
+					"/v1alpha1/escalations",
+					test.requestBody,
+				)
+
+				policiesClient = fake.NewSimpleClientset(
+					k8sStateFixtures...,
+				)
+				handler = escalation.NewWebhookHandler(
+					policiesClient.K8sV1alpha1().EscalationPolicies(),
+				)
+			)
+
+			handler.ServeHTTP(responseWriter, request)
+
+			assert.Equal(t, test.wantStatusCode, responseWriter.Code)
+			assert.Equal(t, "application/json", responseWriter.Header().Get("Content-Type"))
+
+			var gotReview admissionv1.AdmissionReview
+
+			err := json.NewDecoder(responseWriter.Body).Decode(&gotReview)
+			require.NoError(t, err)
+
+			// We don't care about the request here, let's drop it to ease assertions.
+			gotReview.Request = nil
+
+			assert.Equal(t, test.wantReview, gotReview)
+		})
+	}
+}
+
+func encodeObject(t *testing.T, object any) *bytes.Buffer {
+	t.Helper()
+
+	var buf bytes.Buffer
+
+	err := json.NewEncoder(&buf).Encode(&object)
+	require.NoError(t, err)
+
+	return &buf
+}
