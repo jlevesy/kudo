464f20375782e6a2c561798c10ab33e894a1b08a
Julien Levesy <jlevesy@gmail.com>
2022-08-05 20:51:06 +0200

    chore: wire an example event handler

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -6,12 +6,17 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"golang.org/x/sync/errgroup"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog/v2"
 
+	kudov1alpha1 "github.com/jlevesy/kudo/pkg/apis/k8s.kudo.dev/v1alpha1"
+	"github.com/jlevesy/kudo/pkg/controllersupport"
 	clientset "github.com/jlevesy/kudo/pkg/generated/clientset/versioned"
+	kudoinformers "github.com/jlevesy/kudo/pkg/generated/informers/externalversions"
 	"github.com/jlevesy/kudo/webhook"
 )
 
@@ -21,6 +26,8 @@ var (
 
 	webhookConfig webhook.ServerConfig
 )
+
+const defaultInformerResyncInterval = 30 * time.Second
 
 func main() {
 	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
@@ -47,13 +54,61 @@ func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer cancel()
 
+	var (
+		kudoInformerFactory = kudoinformers.NewSharedInformerFactory(kudoClientSet, defaultInformerResyncInterval)
+		escalationsInformer = kudoInformerFactory.K8s().V1alpha1().Escalations().Informer()
+		escalationHandler   = controllersupport.NewQueuedEventHandler[kudov1alpha1.Escalation](
+			&logHandler{},
+			kudov1alpha1.KindEscalation,
+			2,
+		)
+	)
+
+	escalationsInformer.AddEventHandler(escalationHandler)
+
 	group, ctx := errgroup.WithContext(ctx)
 
-	group.Go(func() error { return webhook.RunServer(ctx, kudoClientSet, webhookConfig) })
+	klog.Info("Starting informers, waiting for them to warm up...")
+
+	kudoInformerFactory.Start(ctx.Done())
+
+	if ok := cache.WaitForCacheSync(ctx.Done(), escalationsInformer.HasSynced); !ok {
+		klog.Fatal("Cache sync failed, exiting...")
+	}
+
+	klog.Info("Informers warmed up, starting controller...")
+
+	group.Go(func() error {
+		return webhook.RunServer(ctx, kudoClientSet, webhookConfig)
+	})
+
+	group.Go(func() error {
+		escalationHandler.Run(ctx)
+		return nil
+	})
+
+	klog.Info("Controller up and running!")
 
 	if err := group.Wait(); err != nil {
 		klog.Error("Controller reported an error")
 	}
 
 	klog.Info("Exited kudo controller")
+}
+
+type logHandler struct{}
+
+func (logHandler) OnAdd(escalation *kudov1alpha1.Escalation) error {
+	klog.Info("RECEIVED AN ADD ==>", escalation.Name)
+	return nil
+}
+
+func (logHandler) OnUpdate(oldEsc, newEsc *kudov1alpha1.Escalation) error {
+	klog.Info("RECEIVED AN UPDATE ==>", oldEsc.Name)
+	return nil
+}
+
+func (logHandler) OnDelete(esc *kudov1alpha1.Escalation) error {
+	klog.Info("RECEIVED A DELETE ==>", esc.Name)
+	return nil
 }
